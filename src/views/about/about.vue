fd5fa9c9e2aae39b580c40665be7fdb35b282396
Heanes <heanes@163.com>
2019-05-06 15:59:23 +0800

    feat(page style): add about page style

--- src/views/about/about.vue
+++ src/views/about/about.vue
@@ -11,5 +11,9 @@ export default {
 </script>
 
 <style scoped>
-
+  .about{
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,.12), 0 0 6px 0 rgba(0,0,0,.04);
+    background-color: #f7fff4;
+    padding: 10px;
+  }
 </style>
